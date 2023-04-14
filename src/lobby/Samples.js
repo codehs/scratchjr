bdc640e13bcda96fe8409782a79cce65dec809db
Joel Almeida <jalmeida@codehs.com>
2023-04-14 15:08:20 -0700

    load with id from URLParams

--- src/lobby/Samples.js
+++ src/lobby/Samples.js
@@ -2,13 +2,13 @@
 // Samples Screen
 //////////////////////////////////////////////////
 
-import Lobby from './Lobby';
-import OS from '../tablet/OS';
-import IO from '../tablet/IO';
-import MediaLib from '../tablet/MediaLib';
-import ScratchAudio from '../utils/ScratchAudio';
-import Localization from '../utils/Localization';
-import {gn, newHTML} from '../utils/lib';
+import Lobby from "./Lobby";
+import OS from "../tablet/OS";
+import IO from "../tablet/IO";
+import MediaLib from "../tablet/MediaLib";
+import ScratchAudio from "../utils/ScratchAudio";
+import Localization from "../utils/Localization";
+import { gn, newHTML } from "../utils/lib";
 
 let frame;
 // Should ScratchJr projects be saved when the sample project is changed?
@@ -16,39 +16,40 @@ let frame;
 // window.Settings.useStoryStarters
 
 export default class Samples {
-    static init () {
-        frame = gn('htmlcontents');
-        gn('tabicon').ontouchstart = Samples.playHowTo;
-        gn('tabicon').onclick = Samples.playHowTo;
-        var div = newHTML('div', 'samples off', frame);
-        div.setAttribute('id', 'samples');
-        Samples.display('samples');
+    static init() {
+        frame = gn("htmlcontents");
+        gn("tabicon").ontouchstart = Samples.playHowTo;
+        gn("tabicon").onclick = Samples.playHowTo;
+        var div = newHTML("div", "samples off", frame);
+        div.setAttribute("id", "samples");
+        Samples.display("samples");
     }
 
     ////////////////////////////
     // Show Me How
     ////////////////////////////
 
-    static playHowTo (e) {
+    static playHowTo(e) {
         e.preventDefault();
         e.stopPropagation();
-        ScratchAudio.sndFX('tap.wav');
-        window.location.href = 'gettingstarted.html?place=help';
+        ScratchAudio.sndFX("tap.wav");
+        window.location.href =
+            "gettingstarted.html?place=help&item_id=" + window.item_id;
     }
 
     ////////////////////////////
     // Learn Samples
     ////////////////////////////
 
-    static display (key) {
+    static display(key) {
         var files = MediaLib[key];
         var div = gn(key);
         for (var i = 0; i < files.length; i++) {
             Samples.addLink(div, i, files[i]);
             Samples.requestFromServer(i, files[i], displayThumb);
         }
-        function displayThumb (pos, str) {
-            var mt = gn('sample-' + pos);
+        function displayThumb(pos, str) {
+            var mt = gn("sample-" + pos);
             var data = IO.parseProjectData(JSON.parse(str)[0]);
             var name = mt.childNodes[1];
 
@@ -63,52 +64,57 @@ export default class Samples {
         setTimeout(Samples.show, 10);
     }
 
-    static show () {
+    static show() {
         Lobby.busy = false;
         frame.parentNode.scrollTop = 0;
-        gn('samples').className = 'samples on';
+        gn("samples").className = "samples on";
     }
 
-    static loadMe (e, mt) {
+    static loadMe(e, mt) {
         e.preventDefault();
         e.stopPropagation();
-        ScratchAudio.sndFX('tap.wav');
-        OS.analyticsEvent('samples', 'sample_opened', mt.textContent);
+        ScratchAudio.sndFX("tap.wav");
+        OS.analyticsEvent("samples", "sample_opened", mt.textContent);
         var md5 = mt.md5;
-        window.location.href = 'editor.html?pmd5=' + md5 + '&mode='
-            + ((window.Settings.useStoryStarters) ? 'storyStarter' : 'look');
+        window.location.href =
+            "editor.html?item_id=" +
+            window.item_id +
+            "&pmd5=" +
+            md5 +
+            "&mode=" +
+            (window.Settings.useStoryStarters ? "storyStarter" : "look");
     }
 
-    static insertThumbnail (img, data) {
+    static insertThumbnail(img, data) {
         var md5 = data.md5;
         if (md5) {
-            img.style.backgroundImage = 'url(\'' + md5 + '\')';
+            img.style.backgroundImage = "url('" + md5 + "')";
         }
     }
 
-    static addLink (parent, pos, md5) {
-        var tb = newHTML('div', 'samplethumb', parent);
-        tb.setAttribute('id', 'sample-' + pos);
+    static addLink(parent, pos, md5) {
+        var tb = newHTML("div", "samplethumb", parent);
+        tb.setAttribute("id", "sample-" + pos);
         tb.md5 = md5;
-        tb.type = 'samplethumb';
-        var mt = newHTML('div', 'thumb pos' + pos, tb);
-        newHTML('div', 'woodframe', mt);
-        newHTML('div', 'sampleicon', mt);
-        var name = newHTML('p', undefined, tb);
-        name.textContent = 'Sample ' + pos;
+        tb.type = "samplethumb";
+        var mt = newHTML("div", "thumb pos" + pos, tb);
+        newHTML("div", "woodframe", mt);
+        newHTML("div", "sampleicon", mt);
+        var name = newHTML("p", undefined, tb);
+        name.textContent = "Sample " + pos;
     }
 
-    static requestFromServer (pos, url, whenDone) {
+    static requestFromServer(pos, url, whenDone) {
         var xmlrequest = new XMLHttpRequest();
-        xmlrequest.addEventListener('error', transferFailed, false);
+        xmlrequest.addEventListener("error", transferFailed, false);
         xmlrequest.onreadystatechange = function () {
             if (xmlrequest.readyState == 4) {
                 whenDone(pos, xmlrequest.responseText);
             }
         };
-        xmlrequest.open('GET', url, true);
+        xmlrequest.open("GET", url, true);
         xmlrequest.send(null);
-        function transferFailed (e) {
+        function transferFailed(e) {
             e.preventDefault();
             e.stopPropagation();
             // Failed loading
